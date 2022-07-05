328c3aaef18b0ccf83ed3b77a9696a467656a1ee
EmpireII <zvkrein@gmail.com>
2022-07-05 09:55:27 +0200

    Before beginning work on the UI

--- BlockQueue.cs
+++ BlockQueue.cs
@@ -4,7 +4,9 @@ namespace Tetris
 {
     public class BlockQueue
     {
+        // Instantiating an array containing block objects
         private readonly Block[] blocks = new Block[]
+        //adding the different block shapes to the array
         {
             new IBlock(),
             new JBlock(),
@@ -15,5 +17,35 @@ namespace Tetris
 
 
         };
-    }
+
+        private readonly Random random = new Random();
+
+        //property for the next block in the queue 
+        public Block NextBlock { get; private set;}
+
+
+//in the constructor we 
+    public BlockQueue()
+      {
+            NextBlock = RandomBlock();
+        }
+
+//method to return block
+        private Block RandomBlock()
+        {
+            return blocks[random.Next(blocks.Length)];
+        }
+
+        public Block GetAndUpdate()
+        {
+            Block block = NextBlock;
+
+            do
+            {
+                NextBlock = RandomBlock();
+            }
+            while (block.Id == NextBlock.Id);
+            return block;
+        }
+}
 }
